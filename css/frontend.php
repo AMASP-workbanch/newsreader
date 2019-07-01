9b9cb2f0fe265300fc2a5e614140aa506b69537d
aldus <drp@cms-lab.com>
2019-07-01 21:32:24 +0200

    Update frontend.php

--- css/frontend.php
+++ css/frontend.php
@@ -1,10 +1,41 @@
 <?php
 
-// 1
+/**
+ *
+ * @category        page
+ * @package         newsreader
+ * @author          Robert Hase, Matthias Gallas, Dietrich Roland Pehlke (last)
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WBCE 1.4.x, WebsiteBaker 2.12.x
+ * @requirements    PHP 7.1 and higher
+ * @version         0.4.0
+ * @lastmodified    Jul 2019 
+ *
+ */
+ 
+// 1.0
 require dirname(dirname(dirname(__DIR__)))."/config.php";
 
-// 1.1
+// 2.0
 $sTemplate = DEFAULT_TEMPLATE;
+if(isset($_SESSION['PAGE_ID']))
+{
+    // 2.1
+    $aPageValues = newsreader\system\queries::select(
+        TABLE_PREFIX."pages",
+        ["template"],
+        "page_id=".$_SESSION['PAGE_ID'],
+        false
+    );
+    
+    // 2.2
+    if(isset($aPageValues['template']) && ($aPageValues['template'] != "") )
+    {
+        $sTemplate = $aPageValues['template'];
+    }
+}
+
+// 3.0
 $sPath = WB_PATH."/templates/".$sTemplate."/frontend/newsreader/frontend.css";
 
 $sCSSSource = file_get_contents( 
@@ -13,6 +44,6 @@ $sCSSSource = file_get_contents(
     : __DIR__."/frontend.css"
 );
 
-// 2
+// 4.0
 header('Content-Type: text/css');
 echo  $sCSSSource;
