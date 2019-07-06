a1fe8b840ee86dac8dbdecaca3851fc2f69a313b
aldus <drp@cms-lab.com>
2019-07-06 14:32:45 +0200

    cosmetic changes inside save.php

--- save.php
+++ save.php
@@ -32,18 +32,18 @@ require(WB_PATH."/modules/admin.php");
 
 $tan_ok = false;
 if (true === method_exists($admin, 'checkFTAN')) {
-	if (!$admin->checkFTAN()) {
-		$admin->print_header();
-		$admin->print_error($MESSAGE['GENERIC_SECURITY_ACCESS'], WB_URL);
-		$admin->print_footer();
-		exit();
-	} else {
-		// After check print the header
-		$admin->print_header();
-		$tan_ok = true;
-	}
+    if (!$admin->checkFTAN()) {
+        $admin->print_header();
+        $admin->print_error($MESSAGE['GENERIC_SECURITY_ACCESS'], WB_URL);
+        $admin->print_footer();
+        exit();
+    } else {
+        // After check print the header
+        $admin->print_header();
+        $tan_ok = true;
+    }
 } else {
-	$admin->print_header();
+    $admin->print_header();
 }
    
 $lang_file = __DIR__.'/languages/' . LANGUAGE . '.php';
@@ -56,15 +56,15 @@ $oVal = newsreader\xvalidate::getInstance();
 $oVal->strict_looking_inside = "post";
 
 $all_names = array (
-	'uri'			=> array ('type' => 'str',	'default' => NULL,	'range' =>""),
-	'cycle'			=> array ('type' => 'int+', 'default' => 86400, 'range' => array('min'=> 14400, 'max'=> 999999)),
-	'show_image'	=> array ('type' => 'int+', 'default' => 0, 'range' => array('min'=> 0, 'max'=> 1)),
-	'show_desc'		=> array ('type' => 'int+', 'default' => 0, 'range' => array('min'=> 0, 'max'=> 1)),
-	'show_limit'	=> array ('type' => 'int+',	'default' => "10",	'range' => array('min'=> 1, 'max'=> 50)),
-	'coding_from'		=> array ('type' => 'str',  'default' => '--', 'range' => ""),
-	'coding_to' 		=> array ('type' => 'str', 'default' => '--', 'range' => ""),
-	'use_utf8_encode'	=> array ('type' => 'int+', 'default' => 0, 'range' => array('min'=> 0, 'max'=> 1)),
-	'own_dateformat'	=> array ('type' => 'str',	'default' => NULL,	'range' =>"")
+    'uri'           => array ('type' => 'str',  'default' => NULL,  'range' => ""),
+    'cycle'         => array ('type' => 'int+', 'default' => 86400, 'range' => array('min' => 14400,    'max' => 999999)),
+    'show_image'    => array ('type' => 'int+', 'default' => 0,     'range' => array('min' => 0,        'max'=> 1)),
+    'show_desc'     => array ('type' => 'int+', 'default' => 0,     'range' => array('min' => 0,        'max'=> 1)),
+    'show_limit'    => array ('type' => 'int+', 'default' => "10",  'range' => array('min' => 1,        'max'=> 50)),
+    'coding_from'   => array ('type' => 'str',  'default' => '--',  'range' => ""),
+    'coding_to'     => array ('type' => 'str',  'default' => '--',  'range' => ""),
+    'use_utf8_encode'   => array ('type' => 'int+', 'default' => 0, 'range' => array('min '=> 0,        'max' => 1)),
+    'own_dateformat'    => array ('type' => 'str',  'default' => NULL,  'range' =>"")
 );
 
 $all_values = array ();
@@ -76,51 +76,51 @@ foreach($all_names as $item=>&$options)
 
 if (method_exists( $database, "build_and_execute") )
 {
-	$database->build_and_execute(
-		'update',
-		TABLE_PREFIX."mod_newsreader",
-		$all_values,
-		'section_id = '. $section_id
-	);		
+    $database->build_and_execute(
+        'update',
+        TABLE_PREFIX."mod_newsreader",
+        $all_values,
+        'section_id = '. $section_id
+    );
 
 } else {
-	
-	newsreader\system\queries::update(
-	   TABLE_PREFIX."mod_newsreader",
-	   $all_values,
-	   "`section_id`=".$section_id 
-	);
+
+    newsreader\system\queries::update(
+       TABLE_PREFIX."mod_newsreader",
+       $all_values,
+       "`section_id`=".$section_id 
+    );
 }
 
 // get the newsfeed and save it
 $result = newsreader\newsreader::getInstance()->update(
-	$all_values['uri'],
-	$section_id,
-	$all_values['show_image'],
-	$all_values['show_desc'],
-	$all_values['show_limit'],
-	$all_values['coding_from'],
-	$all_values['coding_to'],
-	$all_values['use_utf8_encode']
+    $all_values['uri'],
+    $section_id,
+    $all_values['show_image'],
+    $all_values['show_desc'],
+    $all_values['show_limit'],
+    $all_values['coding_from'],
+    $all_values['coding_to'],
+    $all_values['use_utf8_encode']
 );
 
 if($database->is_error()) {
-	$admin->print_error(mysql_error() . " ".$query, $js_back);
+    $admin->print_error(mysql_error() . " ".$query, $js_back);
 } else {
-	if (!is_array( $result) ) {
-			$admin->print_error( $result . "<br />".$MOD_NEWSREADER_TEXT['RECORDS UNTOUCHED'], $js_back);
-	
-	} else {
-	    if(isset($_POST['job']))
-	    {
-	        if($_POST['job']== "save_back")
-	        {
-	            $admin->print_success($MESSAGE['PAGES_SAVED'], ADMIN_URL.'/pages/index.php');
-	        } else {
-	            $admin->print_success($MESSAGE['PAGES_SAVED'], ADMIN_URL.'/pages/modify.php?page_id='.$page_id);
-	        }
-	    }
-	}
+    if (!is_array( $result) ) {
+        $admin->print_error( $result . "<br />".$MOD_NEWSREADER_TEXT['RECORDS UNTOUCHED'], $js_back);
+
+    } else {
+        if(isset($_POST['job']))
+        {
+            if($_POST['job']== "save_back")
+            {
+                $admin->print_success($MESSAGE['PAGES_SAVED'], ADMIN_URL.'/pages/index.php');
+            } else {
+                $admin->print_success($MESSAGE['PAGES_SAVED'], ADMIN_URL.'/pages/modify.php?page_id='.$page_id);
+            }
+        }
+    }
 }
 
 $admin->print_footer();
