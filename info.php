e77cb68d8ceae9b889e4cb175d0edcbf6a4d98ff
aldus <drp@cms-lab.com>
2019-07-10 14:35:09 +0200

    Update info.php

--- info.php
+++ info.php
@@ -13,6 +13,17 @@
  *
  */
 
+$module_directory   = "newsreader";
+$module_name        = "Newsreader";
+$module_function    = "page";   // [1]
+$module_version     = "0.4.0";
+$module_platform    = "2.12.2"; // [2]
+$module_author      = "Robert Hase, adm_prg[AT]muc-net.de, Matthias Gallas, Dietrich Roland Pehlke (last)";
+$module_license     = "GNU General Public License";
+$module_description = "This module handels XML and RDF/RSS (0.9x, 1.0, 2.0) newsfeeds.";
+$module_guid        = "913D804E-E254-4E09-A8F4-9C78519EF13D";
+
+//  Overwrite vars
 if(class_exists("addon\\newsreader\\classes\\newsreaderInit", true))
 {
     addon\newsreader\classes\newsreaderInit::getInstance();
@@ -21,12 +32,7 @@ if(class_exists("addon\\newsreader\\classes\\newsreaderInit", true))
     newsreader\system\preload::initialize();            // [2]?
 }
 
-$module_directory   = "newsreader";
-$module_name        = "Newsreader";
+// [1]
 $module_function    = "page".( newsreader\system\core::isWBCE() ? " , preinit" : "");
-$module_version     = "0.4.0";
+// [2]
 $module_platform    = newsreader\system\core::getPlatformVersion();
-$module_author      = "Robert Hase, adm_prg[AT]muc-net.de, Matthias Gallas, Dietrich Roland Pehlke (last)";
-$module_license     = "GNU General Public License";
-$module_description = "This module handels XML and RDF/RSS (0.9x, 1.0, 2.0) newsfeeds.";
-$module_guid        = "913D804E-E254-4E09-A8F4-9C78519EF13D";
